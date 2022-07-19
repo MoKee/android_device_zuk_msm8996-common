817745b8d52254ea8b6f0d8c9b63075e3861e4ef
Martincz Gao <martincz@mokeedev.com>
2022-07-19 14:09:42 +0800

    msm8996-common: mokee-ify
    Change-Id: I01684447cd5beb830675dfc5f0986caf2a86ce7d

--- msm8996.mk
+++ msm8996.mk
@@ -32,11 +32,11 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-mokee
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-lineage/packages/apps/LineageParts
+    $(LOCAL_PATH)/overlay-mokee/packages/apps/MoKeeParts
 
 # Screen density
 PRODUCT_AAPT_CONFIG := normal
@@ -301,7 +301,7 @@ PRODUCT_PACKAGES += \
 
 # LiveDisplay
 PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.0-service-sdm
+    vendor.mokee.livedisplay@2.0-service-sdm
 
 # Media
 PRODUCT_COPY_FILES += \
@@ -406,11 +406,11 @@ PRODUCT_PACKAGES += \
 
 # Touch HAL
 PRODUCT_PACKAGES += \
-    vendor.lineage.touch@1.0-service.zuk_8996
+    vendor.mokee.touch@1.0-service.zuk_8996
 
 # Trust HAL
 PRODUCT_PACKAGES += \
-    vendor.lineage.trust@1.0-service
+    vendor.mokee.trust@1.0-service
 
 # USB
 PRODUCT_PACKAGES += \
